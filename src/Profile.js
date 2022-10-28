ea9cbf7076dca402d96104b114d1057e97e3657d
wafabh95 <77151985+wafabh95@users.noreply.github.com>
2022-10-28 11:42:07 +0200

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -4,9 +4,11 @@ import { Link, Outlet } from 'react-router-dom'
 function Profile() {
   return (
     <div>
+    <h2>hello</h2>
         <h3> Welcome to Your Profile </h3>   
         <Link to='settings'> update your profile </Link> 
         <Outlet/>
+    
         
     </div>
   )
